76dd98e80e1ab2ecc5d8e359045b753dc5b1779c
lyq <920252120@qq.com>
2022-03-25 00:08:04 +0800

    大致能完成项目

--- datanalyze/concat_files.py
+++ datanalyze/concat_files.py
@@ -8,13 +8,17 @@ def concat_csv(path):
     num_files = len(files)
     obj_file = os.path.join(path, files[0])
     result = pd.read_csv(obj_file)
+    print(len(files))
 
 
     for i in range(1, num_files):
+        print(files[i])
         obj_file = os.path.join(path, files[i])
         file = pd.read_csv(obj_file)
         result = pd.concat([result, file])
-    return result
+    result = result.rename(columns={"Unnamed: 0":"date"})
+    print(result)
+    return result.sort_values(by="date")
 
 # obj_file0 = os.path.join(path, files[0])
 # obj_file1 = os.path.join(path, files[1])
@@ -24,7 +28,7 @@ def concat_csv(path):
 # data1 = pd.concat([file1, file0])
 # data1 = data1.sort_values(by='index')
 if __name__ == '__main__':
-    path = r"C:\Users\LYQ\Desktop\datanalyze\to_all0-100\1"
+    path = r"C:\Users\lyq92\Desktop\DataAnalyse\to_all0-100\1"
     # for i in range(2016, 2019):
     #     file_path = path + '\\' + str(i) + 'yearflow.csv'
     #     file = concat_csv(file_path)
--- datanalyze/readfile.py
+++ datanalyze/readfile.py
@@ -1,66 +1,82 @@
 import pandas as pd
+import numpy as np
+from datetime import datetime
+
+
+cp_data_month = []
+for j in range(100):
+    if j== 2:
+        continue
+    if j == 15:
+        continue
+    if j == 18:
+        continue
+    if j == 24:
+        continue
+    if j == 35:
+        continue
+    if j == 39:
+        continue
+    if j == 40:
+        continue
+    if j == 41:
+        continue
+    if j == 42:
+        continue
+    if j == 57:
+        continue
+    if j == 65:
+        continue
+    if j == 72:
+        continue
+    if j == 76:
+        continue
+    if j == 91:
+        continue
+    if j == 99:
+        continue
+    data = []
+    list = ['f', 'pl', 't', 'p', 'tot', 'md', 'ye', 'recTime', 'thetime']
+    for i in range(10):
+        # path = 'C:\\Users\\LYQ\\Desktop\\datanalyze\\all0-100\\' + str(j + 1) + '\\rw' + str(
+        #     j + 1) + '-2021-' + str(i + 1) + '.csv'
+        # to_path = r'C:\Users\LYQ\Desktop\datanalyze\to_all0-100' + '\\' +  str(j + 1) \
+        # + '\\rw' + str(j + 1) + '-2021-' + str(i + 1) + '.csv'
+        path = r'C:\Users\lyq92\Desktop\DataAnalyse\all0-100' +'\\'+str(j+1)\
+                +'\\rw' + str(j + 1) + '-2021-' + str(i + 1) + '.csv'
+
+        to_path = r'C:\Users\lyq92\Desktop\DataAnalyse\to_all0-100' + '\\' + str(j + 1) \
+             + '\\rw' + str(j + 1) + '-2021-' + str(i + 1) + '.csv'
+
+
+        df_data = pd.read_csv(path, header=None, sep='\\t', engine='python')
+        df_data.columns = list
+        df_data = df_data[['tot', 'recTime', 'thetime']]
+        pat = "(00:01:\d\d)|(01\s00:00:)"
+        mask = df_data['thetime'].str.contains(pat)
+        df_data = df_data[mask]
+        data = str(i+1)+r'/1/2021'
+        daily_flow = pd.DataFrame(index=pd.date_range(data, periods=len(df_data)-1))
+        flow = []
+        for z in range(len(df_data)-1):
+            flow.append(df_data.iloc[z+1]['tot'] - df_data.iloc[z]['tot'])
+        flow = np.asarray(flow)
+        daily_flow.insert(column='flow', value=flow, loc=0)
+        print(daily_flow)
+        daily_flow.to_csv(to_path)
 
 
 
 
-def load_data():
-    cp_data_month = []
-    for j in range(100):
-        if j == 1:
-            break
-        if j== 2:
-            continue
-        if j == 15:
-            continue
-        if j == 18:
-            continue
-        if j == 24:
-            continue
-        if j == 35:
-            continue
-        if j == 39:
-            continue
-        if j == 40:
-            continue
-        if j == 41:
-            continue
-        if j == 42:
-            continue
-        if j == 57:
-            continue
-        if j == 65:
-            continue
-        if j == 72:
-            continue
-        if j == 76:
-            continue
-        if j == 91:
-            continue
-        if j == 99:
-            continue
-        data = []
-        list = ['f', 'pl', 't', 'p', 'tot', 'md', 'ye', 'recTime', 'thetime']
-        for i in range(1):
-            path = 'C:\\Users\\LYQ\\Desktop\\datanalyze\\all0-100\\' + str(j + 1) + '\\rw' + str(
-                j + 1) + '-2021-' + str(i + 1) + '.csv'
-            to_path = r'C:\Users\LYQ\Desktop\datanalyze\to_all0-100' + '\\' +  str(j + 1) \
-            + '\\rw' + str(j + 1) + '-2021-' + str(i + 1) + '.csv'
+
+        # df_data.to_csv(to_path)
 
 
-            df_data = pd.read_csv(path, header=None, sep='\\t', engine='python')
-            df_data.columns = list
-            print(df_data[['f', 't', 'p', 'tot', 'recTime', 'thetime']])
-            df_data.to_csv(to_path)
-        #     aa = df_data.values[-1, 4] - df_data.values[0, 4]
-        #     if aa < 0:
-        #         aa = aa1
-        #
-        #     data.append(aa)
-        #     aa1 = aa
-        # cp_data_month.append(data)
+    #     aa = df_data.values[-1, 4] - df_data.values[0, 4]
+    #     if aa < 0:
+    #         aa = aa1
+    #
+    #     data.append(aa)
+    #     aa1 = aa
+    # cp_data_month.append(data)
 
-    return (cp_data_month)
-
-if __name__ == '__main__':
-    data = load_data()
-    print(data)
